532afc5ecac70d7c63823d9b217bdbeeb080d9fc
Facundo Delgado <delgadofacundo071@gmail.com>
2026-07-29 10:59:54 -0300

    Agrego mensaje sobre Git

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main (){
     cout << "Ingrese su nombre: "; cin >> nombre;
     cout << "Hola, " << nombre << endl;
     cout << "Proyecto subido correctamente a GitHub" << endl;
+    cout << "Estoy aprendiendo Git" << endl;
     return 0;
 }
