301cee9df089e74237115b0e264014c855ee06fc
Facundo Delgado <delgadofacundo071@gmail.com>
2026-07-28 23:57:55 -0300

    Agrego mensaje de confirmacion

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main (){
     string nombre;
     cout << "Ingrese su nombre: "; cin >> nombre;
     cout << "Hola, " << nombre << endl;
+    cout << "Proyecto subido correctamente a GitHub" << endl;
     return 0;
 }
