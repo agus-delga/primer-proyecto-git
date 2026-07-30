b2a82307fe26d105195d649985b7bcb7f38a07d1
Facundo Delgado <delgadofacundo071@gmail.com>
2026-07-30 18:52:40 -0300

    Agrego ingreso de ciudad

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 using namespace std;
 int main (){
     string nombre;
+    string ciudad;
     int edad;
     cout << "Ingrese su nombre: ";
     cin >> nombre;
     cout << "Ingrese su edad: ";
     cin >> edad;
+    cout << "Ingrese su ciudad: ";
+    cin >> ciudad;
     cout << "Hola, " << nombre << endl;
     cout << "Tienes " << edad << " anios" << endl;
+    cout << "Vives en " << ciudad << endl;
     return 0;
 }
