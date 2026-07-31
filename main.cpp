0700bf6e972d525751672f00ceb43e37f3a8ad9c
Facundo Delgado <delgadofacundo071@gmail.com>
2026-07-31 16:53:39 -0300

    Min y Max

--- main.cpp
+++ main.cpp
@@ -1,19 +1,54 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
 using namespace std;
-int main (){
-    string nombre;
-    string ciudad;
-    int edad;
-    cout << "Ingrese su nombre: ";
-    cin >> nombre;
-    cout << "Ingrese su edad: ";
-    cin >> edad;
-    cout << "Ingrese su ciudad: ";
-    cin >> ciudad;
-    cout << "Hola, " << nombre << endl;
-    cout << "Tienes " << edad << " anios" << endl;
-    cout << "Vives en " << ciudad << endl;
+
+int main() {
+    vector<int> numeros;
+    int cantidad;
+    int numero;
+
+    cout << "Cuantos numeros desea ingresar: ";
+    cin >> cantidad;
+
+    if (cantidad <= 0) {
+        cout << "La cantidad debe ser mayor que cero" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < cantidad; i++) {
+        cout << "Ingrese el numero " << i + 1 << ": ";
+        cin >> numero;
+
+        numeros.push_back(numero);
+    }
+
+    int suma = 0;
+
+    for (int numero : numeros) {
+        suma += numero;
+    }
+
+    double promedio = static_cast<double>(suma) / numeros.size();
+
+    int minimo = numeros[0];
+    int maximo = numeros[0];
+
+    for (int numero : numeros) {
+        if (numero < minimo) {
+            minimo = numero;
+        }
+
+        if (numero > maximo) {
+            maximo = numero;
+        }
+    }
+
+    cout << "\nResultados:" << endl;
+    cout << "Suma: " << suma << endl;
+    cout << "Promedio: " << promedio << endl;
+    cout << "Minimo: " << minimo << endl;
+    cout << "Maximo: " << maximo << endl;
+
     return 0;
 }
